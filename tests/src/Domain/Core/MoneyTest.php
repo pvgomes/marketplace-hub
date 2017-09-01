bd6ebe1fb0a595017d04091c795ab90805a652d5
Paulo Victor Leite Lima Gomes <paulo.gomes@natue.com.br>
2017-08-31 22:28:54 -0300

    hub interaction testes

---
+++ tests/src/Domain/Core/MoneyTest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace AppBundle\Domain\Core;
+
+use AppBundle\Domain\Core;
+
+
+class MoneyTest extends \PHPUnit_Framework_TestCase
+{
+
+    /**
+     * @test
+     */
+    public function copiedMoneyShouldRepresentSameValue()
+    {
+        $aMoney = new Core\Money(100, new Core\Currency('USD'));
+        $copiedMoney = Core\Money::fromMoney($aMoney);
+        $this->assertTrue($aMoney->equals($copiedMoney));
+    }
+
+    /**
+     * @test
+     */
+    public function originalMoneyShouldNotBeModifiedOnAddition()
+    {
+        $aMoney = new Core\Money(100, new Core\Currency('USD'));
+        $aMoney->add(new Core\Money(20, new Core\Currency('USD')));
+        $this->assertEquals(100, $aMoney->amount());
+    }
+
+    /**
+     * @test
+     */
+    public function moneysShouldBeAdded()
+    {
+        $aMoney = new Core\Money(100, new Core\Currency('USD'));
+        $newMoney = $aMoney->add(new Core\Money(20, new Core\Currency('USD')));
+        $this->assertEquals(120, $newMoney->amount());
+    }
+
+}
---
+++ tests/src/Domain/Core/PriceTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace AppBundle\Domain\Core;
+
+use AppBundle\Domain\Core;
+
+
+class PriceTest extends \PHPUnit_Framework_TestCase
+{
+
+    /**
+     * @test
+     */
+    public function hasSpecialWhenIsLessValue()
+    {
+        $original = new Core\Money(100, new Core\Currency('BRL'));
+        $special = new Core\Money(80, new Core\Currency('BRL'));
+        $price = new Core\Price($original, $special);
+        $this->assertTrue($price->hasSpecial());
+    }
+
+    /**
+     * @test
+     */
+    public function notHasSpecialWhenIsBiggerValue()
+    {
+        $special = new Core\Money(100, new Core\Currency('BRL'));
+        $original = new Core\Money(80, new Core\Currency('BRL'));
+        $price = new Core\Price($original, $special);
+        $this->assertFalse($price->hasSpecial());
+    }
+
+}
